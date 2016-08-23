ce47ebae2c74ffee46bf6934a721d343c3044654
Sam Ghanekar <Sghaneka@yahoo.com>
2016-08-23 17:57:03 -0400

    updating

---
+++ Explorer.Test/Async/AsyncAwaitTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Explorer.Test.Async
+{
+    [TestClass]
+    public class AsyncAwaitTests
+    {
+        // In asynchronous programming, the async method can have three possible return types. These are void, Task, Task<Result>
+
+        [TestMethod]
+        public void AsyncAwaitTest1()
+        {
+            btnCallMethod_Click();
+            Console.WriteLine("Callback has been called");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("I have slept 1 second");
+        }
+
+        // Async void is typically used on event methods, this is where you don't wait for the return from the method
+        public async void btnCallMethod_Click()
+        {
+            await Task.Run(() => { Thread.Sleep(2000); });
+            Console.WriteLine("Done with first task!");
+
+            await Task.Run(() => { Thread.Sleep(2000); });
+            Console.WriteLine("Done with second task!");
+
+            await Task.Run(() => { Thread.Sleep(2000); });
+            Console.WriteLine("Done with third task!");
+
+
+        }
+    }
+}
